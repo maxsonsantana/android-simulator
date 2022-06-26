3eac83ad4f7a0f7612167e311012aa3014b8b297
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-06-26 00:02:59 -0300

    Layouts App

---
+++ app/src/main/java/maxson/com/br/maxbegin/DetailActivity.kt
@@ -0,0 +1,17 @@
+package maxson.com.br.maxbegin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import maxson.com.br.maxbegin.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
