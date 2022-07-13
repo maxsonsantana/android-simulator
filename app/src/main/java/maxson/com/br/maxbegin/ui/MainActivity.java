ec7d1fb9844c7dc3e441c70bda5c876fdf3d0649
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-07-12 23:44:46 -0300

    Crashlytics - Final...

--- app/src/main/java/maxson/com/br/maxbegin/ui/MainActivity.java
+++ app/src/main/java/maxson/com/br/maxbegin/ui/MainActivity.java
@@ -78,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
                    }
                 }
             });
+            //throw new RuntimeException("Teste Crash");
         });
     }
 
